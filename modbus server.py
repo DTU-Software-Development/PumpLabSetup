d33a538c80dc9b908ab44d769f7ec6a6c80e6992
kab_dtu <123185580+DarthSkinny@users.noreply.github.com>
2024-06-24 14:03:50 +0200

    Add files via upload

---
+++ modbus server.py
@@ -0,0 +1,35 @@
+import random
+import logging
+from pymodbus.datastore import ModbusSequentialDataBlock, ModbusSlaveContext, ModbusServerContext
+from pymodbus.server.async_io import StartTcpServer
+
+# Enable logging (makes it easier to debug if something goes wrong)
+logging.basicConfig()
+log = logging.getLogger()
+log.setLevel(logging.DEBUG)
+
+# Define the Modbus registers and coils
+coils = ModbusSequentialDataBlock(1, [False] * 100)
+discrete_inputs = ModbusSequentialDataBlock(1, [False] * 100)
+holding_registers = ModbusSequentialDataBlock(1, [0] * 100)
+input_registers = ModbusSequentialDataBlock(1, [0] * 100)
+
+# Setting a group of registers with random values
+temperature_values = [random.randint(4, 15) for _ in range(7)]
+holding_registers.setValues(1, temperature_values)
+print("temperature_values:", temperature_values)
+
+
+# Define the Modbus slave context
+slave_context = ModbusSlaveContext(
+    di=discrete_inputs,
+    co=coils,
+    hr=holding_registers,
+    ir=input_registers
+)
+
+# Define the Modbus server context
+server_context = ModbusServerContext(slaves=slave_context, single=True)
+
+# Start the Modbus TCP server
+StartTcpServer(context=server_context, address=("localhost", 29172))
